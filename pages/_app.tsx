333c6e8dfbba615f9dcbceafcf5248079666d390
FebriRo <pebri.tambunan@gmail.com>
2026-03-10 14:16:08 +0700

    fix typescript app props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
+import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
           rel="stylesheet"
