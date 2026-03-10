9ea4ed11a70941f6540e8b7c772a5494773d3426
FebriRo <pebri.tambunan@gmail.com>
2026-03-10 14:18:16 +0700

    fix typescript app props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
       </Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+
       <Component {...pageProps} />
     </>
   );
