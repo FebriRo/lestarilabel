c3ef40f4fcb03c5e0d19fd96aefced9e650994f0
FebriRo <pebri.tambunan@gmail.com>
2026-03-12 10:52:48 +0700

    fix scroll thumbnail

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from "next/app"
 import { useEffect } from "react"
 import { useRouter } from "next/router"
 import Head from "next/head"
-
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
